195c6f37ede553a9f80166a34a9104b3c9f49ced
Stephen <smburu@uonbi.ac.ke>
2020-05-24 00:21:45 +0300

    Add Azure deployment files

--- aho_azure/aho_azure/settings.py
+++ aho_azure/aho_azure/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bk080@%e&vd+7xct5cl6nxkm9uw*)gsxat1keb9rj^(4lp5e5v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
